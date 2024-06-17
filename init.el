f8eee203d4e97bb1a2ef1692b159416b3e0d59e1
tokisuno <lucaswmei@gmail.com>
2024-06-17 12:18:22 -0400

    first commit

---
+++ init.el
@@ -0,0 +1,15 @@
+(org-babel-load-file "~/.config/emacs/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(dashboard evil-collection evil general which-key doom-modeline doom-themes nerd-icons-ivy-rich nerd-icons-dired nerd-icons-completion nerd-icons-ibuffer nerd-icons ivy-rich counsel projectile command-log-mode vterm-toggle org-bullets))
+ '(warning-suppress-types '((use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
